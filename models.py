6fa912e1317fde4ff2f2391caacbe3701610a880
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2025-03-01 20:36:24 +0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    last_name = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    faculty = Column(String, nullable=False)
+    course = Column(String, nullable=False)
+    grade = Column(Integer, nullable=False)
